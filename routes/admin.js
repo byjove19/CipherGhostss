4354db4fed0861830ce7a22634a9bc6f34599d83
byjove19 <odionyejovy@gmail.com>
2025-05-15 11:00:13 +0100

    Api Integration

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/Post');
+const Post = require('../models/post');
 const ensureAuthenticated = require('../middleware/authMiddleware');
 
 // Create a new post
